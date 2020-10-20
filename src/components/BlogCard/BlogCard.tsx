d1f938c1a54e7385ab260a2ed29a095d88cf5774
ahmadx16 <ahmadx16@hotmail.com>
2020-10-20 23:58:17 +0500

    initial edit and deleteblog

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -2,32 +2,43 @@ import React from "react";
 import TimeAgo from "react-timeago";
 import { useHistory } from "react-router-dom";
 
-const BlogCard = ({ blogId,title, markdown, date_created }: any) => {
-  
+const BlogCard = ({ blogId, title, markdown, date_created }: any) => {
   let history = useHistory();
-  
+
   const onClickViewMore = (e: any) => {
-    e.preventDefault()
-    history.push('/blog/'+blogId)
+    e.preventDefault();
+    history.push("/blog/" + blogId);
   };
 
   return (
-      <div className="card mt-3">
-        <div className="card-header">Featured</div>
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">
-            {markdown}
-            ...
-          </p>
-          <button className="btn btn-outline-primary" onClick={onClickViewMore}>
-            View More...
-          </button>
-        </div>
-        <div className="card-footer text-muted">
-          <TimeAgo date={date_created} minPeriod={30} />
+    <div className="card mt-3">
+      <div className="card-header">
+        <div className="row">
+          <div className="col-10">Featured</div>
+          <div className="col-2">
+            <button className="btn btn-outline-danger">
+              Delete
+            </button>
+            <button className="btn btn-outline-info ml-2 ">
+              Edit
+            </button>
+          </div>
         </div>
       </div>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">
+          {markdown}
+          ...
+        </p>
+        <button className="btn btn-outline-primary" onClick={onClickViewMore}>
+          View More...
+        </button>
+      </div>
+      <div className="card-footer text-muted">
+        <TimeAgo date={date_created} minPeriod={30} />
+      </div>
+    </div>
   );
 };
 
